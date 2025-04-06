34d9c47b838da73861c882b03fe4c6e2016e2703
stanlaura <93782291+laura-maria-stan@users.noreply.github.com>
2025-04-06 13:52:40 +0300

    Added Player class

---
+++ Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stanb on 29.03.2025.
+//
+
+#include "Player.h"
---
+++ Player.h
@@ -0,0 +1,24 @@
+//
+// Created by stanb on 29.03.2025.
+//
+
+#ifndef OOP_PLAYER_H
+#define OOP_PLAYER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+
+class Player {
+
+
+
+
+
+};
+
+
+#endif //OOP_PLAYER_H
